4e576273da0cf4f23e3cea741b162a637724ee66
Valentin Niess <niess@in2p3.fr>
2017-12-21 15:26:29 +0100

    Add a put method for irods

--- iconnect.py
+++ iconnect.py
@@ -7,13 +7,14 @@ import os
 import subprocess
 
 from irods.exception import (CATALOG_ALREADY_HAS_ITEM_BY_THAT_NAME,
+                             CAT_NAME_EXISTS_AS_COLLECTION,
                              CollectionDoesNotExist, DataObjectDoesNotExist,
                              USER_FILE_DOES_NOT_EXIST)
 from irods.session import iRODSSession
 from irods.data_object import irods_basename
 
 
-HOME = "/trend/home/trirods"
+HOME = "/trend/home/trirods/grand"
 
 
 class Connection(cmd.Cmd, object):
@@ -24,7 +25,7 @@ class Connection(cmd.Cmd, object):
 
     cursor = None
 
-    class _ParsingError(Exception):
+    class _ConnectionError(Exception):
         pass
 
     def default(self, line):
@@ -62,11 +63,11 @@ class Connection(cmd.Cmd, object):
             opts, args = getopt.getopt(args, options)
         except getopt.GetoptError as e:
             self.println("... {:}: {:}", command, e.msg)
-            raise self._ParsingError()
+            raise self._ConnectionError()
 
         if (not noargs) and (not args):
             self.println("... {:}: missing operand", command)
-            raise self._ParsingError()
+            raise self._ConnectionError()
         return opts, args
 
     def println(self, text, *opts):
@@ -80,13 +81,23 @@ class Connection(cmd.Cmd, object):
             text = str(text)
         print self.prompt + text,
 
+    def ask_for_confirmation(self, text, *args):
+        self.printfmt(text, *args)
+        try:
+            answer = raw_input()
+        except EOFError:
+            return False
+        if answer in ("y", "Y", "yes", "Yes"):
+            return True
+        return False
+
     def do_cd(self, line):
         """Change the current irods collection
         """
         # Parse the new path
         try:
             opts, args = self.parse_command("cd", "", line, noargs=True)
-        except self._ParsingError:
+        except self._ConnectionError:
             return
         if not args:
             path = HOME
@@ -111,7 +122,7 @@ class Connection(cmd.Cmd, object):
         """
         try:
             opts, args = self.parse_command("ls", "", line, noargs=True)
-        except self._ParsingError:
+        except self._ConnectionError:
             return
         if not args:
             args = ("*",)
@@ -133,7 +144,7 @@ class Connection(cmd.Cmd, object):
     def do_mkdir(self, line):
         try:
             opts, args = self.parse_command("mkdir", "", line)
-        except self._ParsingError:
+        except self._ConnectionError:
             return
 
         for arg in args:
@@ -153,17 +164,20 @@ class Connection(cmd.Cmd, object):
 
     def do_rm(self, line):
         try:
-            opts, args = self.parse_command("rm", "rf", line)
-        except self._ParsingError:
+            opts, args = self.parse_command("rm", "rfT", line)
+        except self._ConnectionError:
             return
 
         protect_collections = True
         request_confirmation = True
+        skip_trash = False
         for opt, param in opts:
             if opt == "-r":
                 protect_collections = False
             elif opt == "-f":
                 request_confirmation = False
+            elif opt == "-T":
+                skip_trash=True
 
         for arg in args:
             # Check that the object exist and what is its type
@@ -191,17 +205,16 @@ class Connection(cmd.Cmd, object):
 
             # Check for a confirmation
             if request_confirmation:
-                self.printfmt("rm: remove {:} `{:}'?", itype, basename)
-                try:
-                    answer = raw_input()
-                except EOFError:
-                    return True
-                if answer not in ("y", "Y", "yes", "Yes"):
+                if not self.ask_for_confirmation(
+                    "rm: remove {:} `{:}'?", itype, basename):
                     continue
 
             # Now we can remove the data
             try:
-                self.session.collections.remove(path)
+                if itype == "collection":
+                    self.session.collections.remove(path)
+                else:
+                    self.session.data_objects.unlink(path, force=skip_trash)
             except USER_FILE_DOES_NOT_EXIST:
                 self.println("... rm: cannot remove object `{:}`:"
                              "No such data or collection", basename)
@@ -209,6 +222,70 @@ class Connection(cmd.Cmd, object):
 
     def complete_rm(self, text, line, begidx, endidx):
         return self.get_content(text + "*").keys()
+
+    def do_put(self, line):
+        try:
+            opts, args = self.parse_command("rm", "rf", line)
+        except self._ConnectionError:
+            return
+
+        recursive = False
+        request_confirmation = True
+        for opt, param in opts:
+            if opt == "-r":
+                recursive = True
+            elif opt == "-f":
+                request_confirmation = False
+
+        # Parse the src(s) and the destination
+        if len(args) == 1:
+            srcs = args
+            dst = self.cursor.path
+        else:
+            if len(args) == 2:
+                srcs = (args[0],)
+            else:
+                srcs = args[:-1]
+            dst = self.get_path(args[-1])
+
+        # Check if the destination is an existing collection
+        if self.session.collections.exists(dst):
+            if not dst.endswith("/"):
+                dst += "/"
+        elif len(srcs) > 1:
+            self.println("... put: target `{:}` is not a directory", basename)
+            return
+
+        # Upload the data
+        def upload(srcs, dst):
+            for src in srcs:
+                basename = os.path.basename(src)
+                if dst.endswith("/"):
+                    target = dst + basename
+                else:
+                    target = dst
+
+                if os.path.isdir(src):
+                    if not self.session.collections.exists(target):
+                        self.session.collections.create(target)
+                    children = [os.path.join(src, f) for f in os.listdir(src)]
+                    upload(children, target + "/")
+                else:
+                    if self.session.data_objects.exists(target):
+                        if request_confirmation:
+                            if not self.ask_for_confirmation(
+                                "put: overwrite data object `{:}'?", basename):
+                                continue
+                    try:
+                        self.session.data_objects.put(src, dst)
+                    except CAT_NAME_EXISTS_AS_COLLECTION:
+                        self.println("... put: `{:}` is an existing collection",
+                                     basename)
+                        raise self._ConnectionError()
+        try:
+            upload(srcs, dst)
+        except self._ConnectionError:
+            return
 
     def do_shell(self, line):
         args = shlex.split(line)
